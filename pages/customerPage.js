90ed84d8931205e420515c4d593e2512a9247693
zyxwulan <wulandarisri268@gmail.com>
2021-12-15 10:48:25 +0700

    add

--- pages/customerPage.js
+++ pages/customerPage.js
@@ -10,19 +10,19 @@ import Offers from '../components/organisms/offers'
 export default function CustomerPage() {
     return (
         <>
-        <Navbar />
-        <OurClients />
-        <CustomerChoose />
-        <h5 className="workTitle">How Product Work?</h5>
-        <ProductWork content="left" img="/images/step1.png" step="1" title="Apply for a product" desc="Completing our easy step-by-step application form gives you access to multiple business with multiple products. We analyse your business through our proprietary credit analysis technology" />
-        <ProductWork content="right" img="/images/step2.png" step="2" title="Pay fully refundable" desc="Product will contactting you instantly to receive payment of the Application Fee. if no offer of product is available, your fee will be fully refunded." />
-        <ProductWork content="left" img="/images/step3.png" step="3" title="Completion" desc="Product will continue to liase between borrower and manager to procees and complete a quick and efficient settlement" />
-        <div className="container text-center">
-            <Button type="touch" />
-        </div>
-        <Offers />
-        <Join />
-        <Footer />
+            <Navbar />
+            <OurClients />
+            <CustomerChoose />
+            <h5 className="workTitle">How Product Work?</h5>
+            <ProductWork content="left" img="/images/step1.png" step="1" title="Apply for a product" desc="Completing our easy step-by-step application form gives you access to multiple business with multiple products. We analyse your business through our proprietary credit analysis technology" />
+            <ProductWork content="right" img="/images/step2.png" step="2" title="Pay fully refundable" desc="Product will contactting you instantly to receive payment of the Application Fee. if no offer of product is available, your fee will be fully refunded." />
+            <ProductWork content="left" img="/images/step3.png" step="3" title="Completion" desc="Product will continue to liase between borrower and manager to procees and complete a quick and efficient settlement" />
+            <div className="container text-center">
+                <Button type="touch" />
+            </div>
+            <Offers />
+            <Join />
+            <Footer />
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Navbar from '../components/organisms/navbar'
-import Hero from '../components/organisms/hero'
-import Products from '../components/organisms/products'
-import Content1 from '../components/organisms/content1'
-import Content2 from '../components/organisms/content2'
-import Quick from '../components/organisms/quick'
-import Strategies from '../components/organisms/strategies'
-import Price from '../components/organisms/price'
-import Clients from '../components/organisms/clients'
-import Join from '../components/organisms/Join'
-import Footer from '../components/organisms/footer'
+import Navbar from "../components/organisms/navbar";
+import Hero from "../components/organisms/hero";
+import Products from "../components/organisms/products";
+import Content1 from "../components/organisms/content1";
+import Content2 from "../components/organisms/content2";
+import Quick from "../components/organisms/quick";
+import Strategies from "../components/organisms/strategies";
+import Price from "../components/organisms/price";
+import Clients from "../components/organisms/clients";
+import Join from "../components/organisms/Join";
+import Footer from "../components/organisms/footer";
 
 export default function Home() {
   return (
@@ -16,9 +16,37 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Products />
-      <Content1 subtitle="Effortless Validation for" title="Management" desc="The Myspace page defines the individual, his or her characteristics, traits, personal choices and the overall personality of the person." desctitle1="Accessory makers" desc1="While most people enjoy casino gambling, sports betting, lottery and bingo playing for the fun" desctitle2="Alterationists" desc2="If you are looking for a new way to promote your business that won’t cost you more money," image="/images/content1.svg" width={540} height={473}/>
-      <Content2 subtitle="Easier decision making for" title="Customer Support" desc="The Myspace page defines the individual, his or her characteristics, traits, personal choices and the overall personality of the person." />
-      <Content1 subtitle="Optimisation for" title="Collaborative" desc="Few would argue that, despite the advancements of feminism over the past three decades, women still face a double standard when it comes to their behavior. " desctitle1="Accessory makers" desc1="While most people enjoy casino gambling, sports betting, lottery and bingo playing for the fun" desctitle2="Alterationists" desc2="If you are looking for a new way to promote your business that won’t cost you more money," image="/images/content3.svg" width={542} height={451}/>
+      {/* Content 1 */}
+      <Content1
+        subtitle="Effortless Validation for"
+        title="Management"
+        desc="The Myspace page defines the individual, his or her characteristics, traits, personal choices and the overall personality of the person."
+        desctitle1="Accessory makers"
+        desc1="While most people enjoy casino gambling, sports betting, lottery and bingo playing for the fun"
+        desctitle2="Alterationists"
+        desc2="If you are looking for a new way to promote your business that won’t cost you more money,"
+        image="/images/content1.svg"
+        width={540}
+        height={473}
+      />
+      {/* Content 2 */}
+      <Content2
+        subtitle="Easier decision making for"
+        title="Customer Support"
+        desc="The Myspace page defines the individual, his or her characteristics, traits, personal choices and the overall personality of the person."
+      />
+      <Content1
+        subtitle="Optimisation for"
+        title="Collaborative"
+        desc="Few would argue that, despite the advancements of feminism over the past three decades, women still face a double standard when it comes to their behavior. "
+        desctitle1="Accessory makers"
+        desc1="While most people enjoy casino gambling, sports betting, lottery and bingo playing for the fun"
+        desctitle2="Alterationists"
+        desc2="If you are looking for a new way to promote your business that won’t cost you more money,"
+        image="/images/content3.svg"
+        width={542}
+        height={451}
+      />
       <Quick />
       <Strategies />
       <Price />
@@ -26,6 +54,5 @@ export default function Home() {
       <Join />
       <Footer />
     </>
-    
-  )
+  );
 }
